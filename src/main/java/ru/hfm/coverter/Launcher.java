327d3d1ae28f16ad05b04734cefd877494b20261
i.mataras <imataras@yandex.ru>
2017-04-04 18:44:31 +0300

    main calss

---
+++ src/main/java/ru/hfm/coverter/Launcher.java
@@ -0,0 +1,12 @@
+package ru.hfm.coverter;
+
+/**
+ * Created by i.mataras on 04.04.2017.
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Test message");
+    }
+
+}
