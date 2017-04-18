26fc3b808281c4ddaf9a95d23fde5dae4a9653f3
Ivan <imataras@yandex.ru>
2017-04-18 16:28:23 +0300

    start method

--- src/main/java/ru/hfm/console/Launcher.java
+++ src/main/java/ru/hfm/console/Launcher.java
@@ -9,19 +9,25 @@ import ru.hfm.convertor.Convertor;
  * @since 06.04.2017
  */
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Launcher {
 
     private final Convertor convertor;
     private final IO io;
+    private final Map<String, String> parameters;
 
     Launcher(Convertor convertor, IO io) {
         this.convertor = convertor;
         this.io = io;
+        this.parameters = new HashMap<String, String>();
     }
 
     public void start() {
+
+        System.out.println("HFM convertor version 1.0.0.0 has started");
 
         try (Validator validator = new Validator(io)) {
 
@@ -32,7 +38,6 @@ public class Launcher {
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to th main menu");
         new Launcher(new Convertor(), new ConsoleIO(new Scanner(System.in), System.out)).start();
     }
 
