d0fbedb3810bbb1eeb916f1fe7cd218d2e04b195
i.mataras <imataras@yandex.ru>
2017-06-06 11:20:52 +0300

    fix Validator class

--- src/main/java/ru/hfm/convertor/Normalizer.java
+++ src/main/java/ru/hfm/convertor/Normalizer.java
@@ -14,13 +14,13 @@ import java.util.List;
 
 class Normalizer {
 
-    private final String DEFAULT_SourceFMEntity_VALUE = new String("E29000");
+    private final String DEFAULT_SourceFMEntity_VALUE = "E29000";
     private final Integer DEFAULT_SourceFMAccount_VALUE = 0;
-    private final String DEFAULT_SourceICP_VALUE = new String("[ICP None]");
-    private final String DEFAULT_SourceCustom1_VALUE = new String("[None]");
-    private final String DEFAULT_SourceCustom2_VALUE = new String("[None]");
-    private final String DEFAULT_SourceCustom3_VALUE = new String("[None]");
-    private final String DEFAULT_SourceCustom4_VALUE = new String("[None]");
+    private final String DEFAULT_SourceICP_VALUE = "[ICP None]";
+    private final String DEFAULT_SourceCustom1_VALUE = "[None]";
+    private final String DEFAULT_SourceCustom2_VALUE = "[None]";
+    private final String DEFAULT_SourceCustom3_VALUE = "[None]";
+    private final String DEFAULT_SourceCustom4_VALUE = "[None]";
     private final BigDecimal DEFAULT_Amount_VALUE = new BigDecimal(0).setScale(0, RoundingMode.HALF_UP);
 
     private final String DEFAULT_SourceCustom4_LOCSTAT_VALUE = new String("LOCSTAT");
