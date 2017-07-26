48612bbd8276a00c86540bad33bec5b342595954
Ivan <imataras@yandex.ru>
2017-07-27 02:08:46 +0300

    Validator class implementation

--- src/main/java/ru/hfm/convertor/Writer.java
+++ src/main/java/ru/hfm/convertor/Writer.java
@@ -7,7 +7,11 @@ package ru.hfm.convertor;
  * @since 06.04.2017
  */
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 class Writer {
@@ -16,20 +20,57 @@ class Writer {
     private File file;
     private List<DataRecord> dataArray;
 
-    Writer(Parameters parameters, List<DataRecord> dataArray){
+    Writer(Parameters parameters, List<DataRecord> dataArray) {
         this.parameters = parameters;
         this.dataArray = dataArray;
     }
 
-    void setFile(){
+    void setFile() {
         this.file = new File(parameters.getOutputFile());
     }
 
-    void writeFile(){
+    void writeFile() {
 
+        if (!this.file.exists()) {
+            try {
+                this.file.createNewFile();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        } else if (this.file.exists()) {
+            this.file.delete();
+        }
+
+        try (FileWriter fileWriter = new FileWriter(this.file.getAbsoluteFile())) {
+
+            try (BufferedWriter bufferedWriter = new BufferedWriter(fileWriter, 1024 * 1024)) {
+
+                Iterator<DataRecord> dataArrayIterator  = dataArray.iterator();
+
+                while (dataArrayIterator.hasNext()) {
+
+                    DataRecord dataRecord = dataArrayIterator.next();
+
+
+
+                }
+
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
 
     }
 
+    private String constructLine(DataRecord dataRecord) {
+
+        String line = "";
+        return line;
+
+    }
 
 }
